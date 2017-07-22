0c6c6d477f1aca2121cf42242312a0c1417152a4
parkps618 <parkps618@gmail.com>
2017-07-22 14:42:41 -0400

    Gallery Fragment implemented

---
+++ app/src/main/java/com/hanson/geoclick/GalleryAdapter.java
@@ -0,0 +1,62 @@
+package com.hanson.geoclick;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Pyosnag on 2017. 7. 22..
+ */
+
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHolder> {
+    private ArrayList<CreateList> galleryList;
+    private Context context;
+
+    public GalleryAdapter(Context context, ArrayList<CreateList> galleryList) {
+        this.galleryList = galleryList;
+        this.context = context;
+    }
+
+    @Override
+    public GalleryAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cell_layout, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(GalleryAdapter.ViewHolder viewHolder, int i) {
+        viewHolder.title.setText(galleryList.get(i).getImage_title());
+        viewHolder.img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        viewHolder.img.setImageResource((galleryList.get(i).getImage_ID()));
+
+        viewHolder.img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"Image",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return galleryList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView title;
+        private ImageView img;
+        public ViewHolder(View view) {
+            super(view);
+
+            title = (TextView)view.findViewById(R.id.title);
+            img = (ImageView) view.findViewById(R.id.img);
+        }
+    }
+}
--- app/src/main/java/com/hanson/geoclick/GalleryFragment.java
+++ app/src/main/java/com/hanson/geoclick/GalleryFragment.java
@@ -4,13 +4,40 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 
 
 public class GalleryFragment extends Fragment {
+
+    private final String image_titles[] = {
+            "Img1",
+            "Img2",
+            "Img3",
+            "Img4",
+            "Img5",
+            "Img6",
+            "Img7",
+            "Img8",
+    };
+
+    private final Integer image_ids[] = {
+            R.drawable.lion,
+            R.drawable.micky,
+            R.drawable.logowithtitle,
+            R.drawable.fire,
+            R.drawable.lion,
+            R.drawable.micky,
+            R.drawable.logowithtitle,
+            R.drawable.fire,
+
+    };
+
 
     public GalleryFragment() {
         // Required empty public constructor
@@ -26,7 +53,31 @@ public class GalleryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.imagegallery);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity().getApplicationContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<CreateList> createLists = prepareData();
+        GalleryAdapter adapter = new GalleryAdapter(getActivity().getApplicationContext(), createLists);
+        recyclerView.setAdapter(adapter);
+
         return view;
     }
 
+    private ArrayList<CreateList> prepareData(){
+
+        ArrayList<CreateList> theimage = new ArrayList<>();
+        for(int i = 0; i< image_titles.length; i++){
+            CreateList createList = new CreateList();
+            createList.setImage_title(image_titles[i]);
+            createList.setImage_ID(image_ids[i]);
+            theimage.add(createList);
+        }
+        return theimage;
+    }
 }
+
+
+
